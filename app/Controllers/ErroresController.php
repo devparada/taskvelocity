d7fbf196fc21116f3dd7c327997978f5aa358687
Raúl <raul.parada040103@gmail.com>
2024-04-30 22:10:06 +0200

    Modifico ErroresController y decoro la página de error (404 y 405)

--- app/Controllers/ErroresController.php
+++ app/Controllers/ErroresController.php
@@ -10,7 +10,12 @@ class ErroresController extends \Com\Daw2\Core\BaseController {
         http_response_code(404);
         $data = ['titulo' => 'Error 404'];
         $data['texto'] = 'Error 404. URL no encontrada.';
-        $this->view->show('admin/error.php', $data);
+
+        if (isset($_SESSION["usuario"]) && $_SESSION["usuario"]["id_rol"] == 1) {
+            $this->view->showViews(array('admin/templates/header.view.php', 'admin/error.php', 'admin/templates/footer.view.php'), $data);
+        } else {
+            $this->view->show('public/error.php', $data);
+        }
     }
 
     function error405(): void {
@@ -18,6 +23,10 @@ class ErroresController extends \Com\Daw2\Core\BaseController {
         $data = ['titulo' => 'Error 405'];
         $data['texto'] = 'Error 405. Método no disponible.';
 
-        $this->view->show('admin/error.php', $data);
+        if (isset($_SESSION["usuario"]) && $_SESSION["usuario"]["id_rol"] == 1) {
+            $this->view->showViews(array('admin/templates/header.view.php', 'admin/error.php', 'admin/templates/footer.view.php'), $data);
+        } else {
+            $this->view->show('public/error.php', $data);
+        }
     }
 }
---
+++ app/Views/public/error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>TaskVelocity | 404</title>
+        <link rel="stylesheet" href="assets/css/public/estilosGeneral.css">
+        <link rel="stylesheet" href="assets/css/public/estilosError.css">
+    </head>
+    <body>
+        <div>
+            <div id="mensaje-error">
+                <p><?php echo $texto; ?></p>
+            </div>
+            <div id="boton-volver">
+                <a href="/" class="botones">Inicio</a>
+            </div>
+        </div>
+    </body>
+</html>
